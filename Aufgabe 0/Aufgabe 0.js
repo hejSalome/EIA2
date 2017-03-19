e5c28da5947c8fc8a3e631e54abdfb40a03638a8
Salome <Salome@Travelmate.fritz.box>
2017-03-19 15:51:37 +0100

    Aufgabe 0 bearbeiten

--- Aufgabe 0/Aufgabe 0.js
+++ Aufgabe 0/Aufgabe 0.js
@@ -1,11 +1,10 @@
-<!-- 
-Aufgabe: Aufgabe 0 - Arbeisf�higkeit
-Name: Salome Weißer
-Matrikel: 254669
-Datum: 19.03.2017
-	
-Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
--->
+//Aufgabe: Aufgabe 0 - Arbeisfähigkeit
+//Name: Salome Weißer
+//Matrikel: 254669
+//Datum: 19.03.2017
+//	
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
 
 document.addEventListener ('DOMConetentLoaded', function () {
 var name = prompt("Please enter your name");
