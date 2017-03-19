d4ba16a8570c9c8acecf0b4abd83423023a5f626
Salome <Salome@Travelmate.fritz.box>
2017-03-19 16:42:01 +0100

    JS verbessern

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -6,7 +6,7 @@
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 
 
-document.addEventListener ('DOMConetentLoaded', function () {
+document.addEventListener ('DOMContentLoaded', function () {
 var name = prompt("Please enter your name");
 
 if (name != null) {
