6c56ba2dbcb88e472a6ea956613d82167e048db4
Salome <Salome@Travelmate.hfu-open.fu.hs-furtwangen.de>
2017-03-30 12:28:55 +0200

    Schachbrett aktualisieren

--- Aufgabe2/Schachbrett.js
+++ Aufgabe2/Schachbrett.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let rice = 1;
+    //   function placeDivs (): void { 
+    for (let i = 0; i < 64; i++) {
+        let element = document.createElement("div");
+        if (i % 2 == 0) {
+            element.className = "board white";
+        }
+        else {
+            element.className = "board black";
+        }
+        document.body.appendChild(element);
+    }
+});
 //# sourceMappingURL=Schachbrett.js.map
--- Aufgabe2/Schachbrett.ts
+++ Aufgabe2/Schachbrett.ts
@@ -1,14 +1,26 @@
-DOMContentLoaded = function (): void {
-   
-//   function placeDivs (): void { 
+document.addEventListener('DOMContentLoaded', function(): void {
+    let rice: number = 1;
+
+    //   function placeDivs (): void { 
     for (let i: number = 0; i < 64; i++) {
         let element: HTMLElement = document.createElement("div");
-       if (i % 2 == 0) {                             // 0 Modulo 2 w‰re auch 0!
-           element.className = "board white";
-           }
-        else {
-           element.className = "board black";
-           }
-        document.body.appendChild(element);
+        if (i % 2 == 0) {                             // 0 Modulo 2 w‰re auch 0!
+            element.className = "board white";
         }
-    };
+        else {
+            element.className = "board black";
+        }
+        document.body.appendChild(element);
+//        
+//        for (let rice: number = 1, rice ,rice * 2 ) {
+//        placeDiv (rice);
+//       
+//        };
+        
+//        for (let a: number = 65, a < 65, a++) {
+//               let element : 
+//            if (i>0)   placeDiv (rice * 2,          //auﬂer 1.Runde 
+//        } 
+}); 
+
+console.log();
