aff9cbfbd35032d6f08e946e3894d374a450faf3
Salome <Salome@Travelmate.hfu-open.fu.hs-furtwangen.de>
2017-03-30 13:33:29 +0200

    A2 akturalisieren

--- Aufgabe2/Schachbrett.js
+++ Aufgabe2/Schachbrett.js
@@ -3,12 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
     //   function placeDivs (): void { 
     for (let i = 0; i < 64; i++) {
         let element = document.createElement("div");
+        element.textContent = rice.toString();
+        //        
+        //         if (i % 8 == 0) {
+        //            element.className += "clear";
+        //            }
+        console.log("i");
         if (i % 2 == 0) {
             element.className = "board white";
         }
         else {
             element.className = "board black";
         }
+        //        if (i == 9,25,41,57) 
+        //        i%2 == 0 : element.className ="board black";
+        //        i%2 == 1 : element.className ="board white";
+        rice = rice * 2;
         document.body.appendChild(element);
     }
 });
--- Aufgabe2/Schachbrett.ts
+++ Aufgabe2/Schachbrett.ts
@@ -3,24 +3,29 @@ document.addEventListener('DOMContentLoaded', function(): void {
 
     //   function placeDivs (): void { 
     for (let i: number = 0; i < 64; i++) {
+       
         let element: HTMLElement = document.createElement("div");
+        element.textContent = rice.toString(); 
+//        
+//         if (i % 8 == 0) {
+//            element.className += "clear";
+//            }
+        console.log("i");
         if (i % 2 == 0) {                             // 0 Modulo 2 w‰re auch 0!
             element.className = "board white";
         }
         else {
             element.className = "board black";
-        }
-        document.body.appendChild(element);
-//        
-//        for (let rice: number = 1, rice ,rice * 2 ) {
-//        placeDiv (rice);
-//       
-//        };
+        }  
         
-//        for (let a: number = 65, a < 65, a++) {
-//               let element : 
-//            if (i>0)   placeDiv (rice * 2,          //auﬂer 1.Runde 
-//        } 
-}); 
-
-console.log();
+       
+//        if (i == 9,25,41,57) 
+//        i%2 == 0 : element.className ="board black";
+//        i%2 == 1 : element.className ="board white";
+        
+        
+        rice = rice * 2;
+        document.body.appendChild(element);
+     
+       }
+    });
