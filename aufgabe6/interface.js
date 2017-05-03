ddeab0db078934dc0cb3862d84c0b6d69d9eb4a8
Salome <Salome@Travelmate>
2017-05-03 23:04:37 +0200

    Add A6, add Konzept to Steckbrief

---
+++ aufgabe6/interface.js
@@ -0,0 +1,30 @@
+var aufgabe6_interface;
+(function (aufgabe6_interface) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel) {
+        return "Hier fehlt noch der richtige Code...";
+    }
+})(aufgabe6_interface || (aufgabe6_interface = {}));
+//# sourceMappingURL=interface.js.map
