620af6c80a6b464fdb8ec22aa1cfa63e48200325
Salome <Salome@Travelmate>
2017-05-25 14:58:06 +0200

    Create Aufgabe 9

---
+++ aufgabe9/Forms.js
@@ -0,0 +1,36 @@
+var aufgabe9_Forms;
+(function (aufgabe9_Forms) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+        }
+    }
+    function handleChange(_event) {
+        //console.log(_event);
+        //*/
+        let target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+        //*/
+        //*/ note: this == _event.currentTarget in an event-handler
+        if (this.id == "checkbox")
+            console.log("Changed " + target.name + " to " + target.checked);
+        //*/
+        //*/
+        if (target.name == "Slider") {
+            let progress = document.getElementsByTagName("progress")[0];
+            progress.value = parseFloat(target.value);
+        }
+        //*/
+        //*/
+        if (target.name == "Stepper") {
+            let progress = document.getElementsByTagName("meter")[0];
+            progress.value = parseFloat(target.value);
+        }
+        //*/
+    }
+})(aufgabe9_Forms || (aufgabe9_Forms = {}));
+//# sourceMappingURL=Forms.js.map
