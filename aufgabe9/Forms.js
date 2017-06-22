77591d09c6b38491fd515e526657d66a349d016f
Salome <Salome@Travelmate.hfu-open.fu.hs-furtwangen.de>
2017-06-22 11:46:59 +0200

    Add NodeTest and Repo

--- aufgabe9/Forms.js
+++ aufgabe9/Forms.js
@@ -1,8 +1,35 @@
+"use strict";
 //Aufgabe 9
 //Name: Salome Weisser
 //Matrikel: 254669
 //Datum: 03.06.2017
 //
+console.log("Server starting");
+const Http = require("http");
+const Url = require("url");
+let port = process.env.PORT;
+if (port == undefined)
+    port = 8100;
+let server = Http.createServer();
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
+function handleListen() {
+    console.log("Listening on port: " + port);
+}
+function handleRequest(_request, _response) {
+    console.log("Request received");
+    console.log(_request.url);
+    let query = Url.parse(_request.url, true).query;
+    console.log(query);
+    let key;
+    for (key in query)
+        console.log(key + ":" + query[key]);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("Ich höre Stimmen!");
+    _response.end();
+}
 var aufgabe9_Forms;
 (function (aufgabe9_Forms) {
     window.addEventListener("load", init);
