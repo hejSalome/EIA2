4eccb3fcb320e6a96ef8603e464e7270e56881da
Salome <Salome@Travelmate.hfu-open.fu.hs-furtwangen.de>
2017-06-29 11:36:03 +0200

    Add

--- aufgabe11/DatabaseClient.js
+++ aufgabe11/DatabaseClient.js
@@ -23,7 +23,7 @@ var aufgabe11;
     }
     function sendRequest(_query, _callback) {
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "http://localhost:8100?" + _query, true); //Verbindung wird geöffnet; true steht dafür dass es asynchron ist
         //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
         xhr.addEventListener("readystatechange", _callback);
         xhr.send();
